2d4ca7f3e48e6621636151cb14eab372150b09a0
Levin Keller <post@levinkeller.de>
2018-10-18 12:32:24 +0200

    Do not set headers.

--- packages/functions/src/getData.js
+++ packages/functions/src/getData.js
@@ -1,13 +1,6 @@
 exports.handler = (_, __, callback) => {
   return callback(null, {
     statusCode: 200,
-    body: "Success!",
-    headers: {
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Headers": "content-type,origin,text,startlower",
-      "Access-Control-Allow-Methods": "GET, OPTIONS",
-      "content-type": "text/plain",
-      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-    }
+    body: "Success!"
   });
 };
